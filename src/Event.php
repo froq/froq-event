1da6b51020770cd92627b17e08822dd54a1545f6
Kerem Güneş <k-gun@mail.com>
2016-05-06 12:10:19 +0300

    Add Event, Events, EventException objects.

--- src/Event.php
+++ src/Event.php
@@ -1,1 +1,50 @@
 <?php
+namespace Froq\Event;
+
+final class Event
+{
+    private $name;
+    private $func;
+    private $funcArgs = [];
+
+    final public function __construct(string $name, callable $func = null, array $funcArgs = null)
+    {
+        $this->name = $name;
+        if ($func) {
+            $this->setFunc($func);
+        }
+        if ($funcArgs) {
+            $this->setFuncArgs($funcArgs);
+        }
+    }
+
+    final public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+    final public function getName(): string
+    {
+        return $this->name;
+    }
+
+    final public function setFunc(callable $func): self
+    {
+        $this->func = $func;
+        return $this;
+    }
+    final public function getFunc(): callable
+    {
+        return $this->func;
+    }
+
+    final public function setFuncArgs(array $funcArgs): self
+    {
+        $this->funcArgs = $funcArgs;
+        return $this;
+    }
+    final public function getFuncArgs(): array
+    {
+        return $this->funcArgs;
+    }
+}
--- src/Events.php
+++ src/Events.php
@@ -1,1 +1,27 @@
 <?php
+namespace Froq\Event;
+
+use Froq\Collection\Collection;
+
+final class Events extends Collection
+{
+    final public function on(string $name, callable $func, array $funcArgs = null): self
+    {
+        $this->set($name, new Event($name, $func, $funcArgs));
+        return $this;
+    }
+
+    final public function off(string $name): self
+    {
+        $this->del($name);
+        return $this;
+    }
+
+    final public function fire(string $name)
+    {
+        $event = $this->get($name);
+        if ($event) {
+            return call_user_func_array($event->getFunc(), $event->getFuncArgs());
+        }
+    }
+}
