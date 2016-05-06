a0481907346f4dd7445d60648fe2d3ad165c5322
Kerem Güneş <k-gun@mail.com>
2016-05-06 12:24:37 +0300

    Add normalizeName().

--- src/Events.php
+++ src/Events.php
@@ -7,21 +7,29 @@ final class Events extends Collection
 {
     final public function on(string $name, callable $func, array $funcArgs = null): self
     {
+        $name = $this->normalizeName($name);
         $this->set($name, new Event($name, $func, $funcArgs));
         return $this;
     }
 
     final public function off(string $name): self
     {
+        $name = $this->normalizeName($name);
         $this->del($name);
         return $this;
     }
 
     final public function fire(string $name)
     {
+        $name = $this->normalizeName($name);
         $event = $this->get($name);
         if ($event) {
             return call_user_func_array($event->getFunc(), $event->getFuncArgs());
         }
+    }
+
+    final private function normalizeName(string $name): string
+    {
+        return strtolower($name);
     }
 }
