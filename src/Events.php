7820e1b126036612cbccee431e7eaaa9c41aa138
Kerem Güneş <qeremy@gmail.com>
2020-12-23 06:57:51 +0300

    Head doc-ups, doc-ups.

--- src/Events.php
+++ src/Events.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright (c) 2015 · Kerem Güneş
- * Apache License 2.0 <https://opensource.org/licenses/apache-2.0>
+ * Apache License 2.0 · http://github.com/froq/froq-encrypting
  */
 declare(strict_types=1);
 
@@ -14,7 +14,7 @@ use froq\event\{EventException, Event};
  *
  * @package froq\event
  * @object  froq\event\Events
- * @author  Kerem Güneş <k-gun@mail.com>
+ * @author  Kerem Güneş
  * @since   1.0
  */
 final class Events
